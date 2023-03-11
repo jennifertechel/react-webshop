52d2839880f4dd0876f5e55c52a4eae8fbc7c958
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-11 10:49:12 +0100

    updated and added products

--- data/index.ts
+++ data/index.ts
@@ -14,56 +14,83 @@ export interface CartItem extends Product {
 export const products: Product[] = [
   {
     image: "/public/product-images/p-1.svg",
-    title: "Calude",
+    title: "CLAUDE",
     description:
-      "Handmade ceramic vase. The vase is shaped like two hands carrying something and it radiates care. The dark one has a shiny, marbled glaze, the light one has a matte surface with texture. Both colors have a glazed interior. The vase is handmade, making each piece unique. Please note that ceramics may allow moisture to pass through.",
+      "Handgjord vas av keramik. Vasen är formad som två händer som bär upp något och den utstrålar omtanke. Den mörka har en blank, melerad glasyr, den ljusa har matt yta med struktur. Båda färgarna har glacerad insida. Vasen är handgjord vilket gör varje exemplar unikt. Tänk på att keramik kan släppa igenom fukt.",
     price: 300,
     id: "a1",
-    height: "Height: 18 cm",
-  },
-  {
-    image: "/public/product-images/p-2.svg",
-    title: "Ralph",
-    description:
-      "Handmade decorative vase of ceramic. A sleek vase with an exciting shape and ribbed surface. The inside is also glazed. Some variation may occur as the vase is handmade. Ceramics may allow moisture to pass through.",
-    price: 500,
-    id: "b2",
-    height: "Height: 33 cm",
-  },
-  {
-    image: "/public/product-images/p-3.svg",
-    title: "Peyton",
-    description:
-      "Handmade decorative vase of glazed ceramic. PEYTON has a flowing shape and, together with the beautiful glaze, resembles the corals of the sea. The inside is also glazed. The craftsmanship makes each vase unique. Please note that ceramics may allow moisture to pass through.",
-    price: 400,
-    id: "c3",
-    height: "Height: 21 cm",
+    height: "18 cm",
   },
   {
     image: "/public/product-images/p-4.svg",
-    title: "Algot",
+    title: "GASTON",
     description:
-      "Handmade decorative vase of glazed ceramic with a lively, beautifully bubbly glaze and irregular shape. The inside is also glazed. Some variation may occur as the vase is handmade. Ceramics may allow moisture to pass through.",
+      "Handgjord dekorationsvas av glaserad keramik med en livfull, vackert bubblig glasyr och oregelbunden form. Även insidan är glaserad. Viss avvikelse kan förekomma eftersom vasen är handgjord. Keramik kan släppa igenom fukt.",
     price: 500,
     id: "d4",
-    height: "Height: 28,5 cm",
+    height: "28 cm",
+  },
+  {
+    image: "/public/product-images/p-2.svg",
+    title: "RALPH",
+    description:
+      "Handgjord dekorationsvas av keramik. En stilren vas med spännande form och räfflad yta. Även insidan är glaserad. Viss avvikelse kan förekomma eftersom vasen är handgjord. Keramik kan släppa igenom fukt.",
+    price: 500,
+    id: "b2",
+    height: "33 cm",
+  },
+  {
+    image: "/public/product-images/p-11.svg",
+    title: "BAUER",
+    description:
+      "Handgjord dekorationsvas i keramik som för tankarna till havets koraller. Viss avvikelse kan förekomma eftersom vasen är handgjord. Keramik kan släppa igenom fukt.",
+    price: 300,
+    id: "c3",
+    height: "22 cm",
+  },
+  {
+    image: "/public/product-images/p-7.svg",
+    title: "JORDAN",
+    description:
+      "Handgjord vas av keramik med blank, glasyr som är vacker tillsammans med gröna växter. Även insidan är glaserad. Viss avvikelse kan förekomma eftersom vasen är handgjord. Keramik kan släppa igenom fukt.",
+    price: 300,
+    id: "f6",
+    height: "20 cm",
+  },
+  {
+    image: "/public/product-images/p-3.svg",
+    title: "PEYTON",
+    description:
+      "Handgjord dekorationsvas av glaserad keramik. PEYTON är böljande i formen och tillsammans med den vackra glasyren påminner vasen om havets koraller. Även insidan är glaserad. Hantverket gör att varje vas är unik. Tänk på att keramik kan släppa igenom fukt.",
+    price: 400,
+    id: "c3",
+    height: "26 cm",
   },
   {
     image: "/public/product-images/p-5.svg",
-    title: "Atticus",
+    title: "ATTICUS",
     description:
-      "Handmade decorative vase of glazed ceramic. ATTICUS has a flowing shape and, together with the beautiful glaze, resembles the corals of the sea. The inside is also glazed. The craftsmanship makes each vase unique. Please note that ceramics may allow moisture to pass through.",
+      "Handgjord dekorationsvas av glaserad keramik. ATTICUS är böljande i formen och tillsammans med den vackra glasyren påminner vasen om havets koraller. Även insidan är glaserad. Hantverket gör att varje vas är unik. Tänk på att keramik kan släppa igenom fukt.",
     price: 300,
     id: "e5",
-    height: "Height: 21 cm",
+    height: "21 cm",
   },
   {
-    image: "/public/product-images/p-6.svg",
-    title: "Jordan",
+    image: "/public/product-images/p-10.svg",
+    title: "ELTON",
     description:
-      "Handmade ceramic vase that also becomes a decorative object wherever it is placed. The vase is handmade in a beautiful shape and then glazed with an impactful splatter pattern. The fine craftsmanship makes each piece unique. Please note that ceramics may allow moisture to pass through.",
+      "Handgjord vas av keramik med glaserad ut- och insida i ljusa toner. Associerar du med koraller i havet eller en vacker vulkan? Inspirationen till hantverket har hämtats från naturen och hantverket gör varje exemplar unikt. Tänk på att keramik kan släppa igenom fukt.",
     price: 600,
-    id: "f6",
-    height: "Height: 32 cm",
+    id: "e5",
+    height: "34 cm",
+  },
+  {
+    image: "/public/product-images/p-8.svg",
+    title: "LOU",
+    description:
+      "Stilren vas av keramik. LOU är en vas i unik form som blir en spännande detalj i ditt hem. Vasen är handgjord vilket gör varje exemplar unikt. Tänk på att keramik kan släppa igenom fukt.",
+    price: 300,
+    id: "e5",
+    height: "20 cm",
   },
 ];
