9b4afbc93f56ee1118ea7fb2f56066e6c62aa0ea
ye-mou <git.ymo22@gmail.com>
2023-03-12 13:00:37 +0100

    Styled the orderconfirmation

--- src/pages/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation.tsx
@@ -2,8 +2,28 @@ import { Box, Center } from "@chakra-ui/react";
 
 function OrderConfirmation() {
   return (
-    <Center h="100vh">
-      <Box fontSize="xl">Tack för din beställning.</Box>
+    <Center minH="100vh" flexDirection="column">
+      <Box
+        fontSize={{ base: "xl", md: "2xl" }}
+        fontWeight="bold"
+        padding={{ base: "4", md: "8" }}
+        textAlign={{ base: "center", md: "left" }}
+      >
+        <h1>Tack för din beställning.</h1>
+      </Box>
+
+      <Box
+        display="flex"
+        justifyContent="space-evenly"
+        alignItems="center"
+        padding={{ base: "4", md: "8" }}
+        flexDirection="column"
+      >
+        <h2>Produkt(er):</h2>
+        <h2>Antal:</h2>
+        <h2>Pris:</h2>
+        <h2>Ordernummer:</h2>
+      </Box>
     </Center>
   );
 }
