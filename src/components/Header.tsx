dc0c3bdecd1984bfc127ff8be8b57da168d5ae5d
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-13 13:50:06 +0100

    made header fixed with bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,18 @@ import { Center, Flex, Image } from "@chakra-ui/react";
 
 function Header() {
   return (
-    <Flex as="header" p={4} borderBottom="1px" borderColor="blackAlpha.200">
+    <Flex
+      as="header"
+      p={4}
+      borderBottom="1px"
+      borderColor="blackAlpha.200"
+      bg="brand.100"
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      zIndex="sticky"
+    >
       <Center width="100vw">
         <Image src="logo.svg" maxWidth="150px" maxHeight="100%" />
       </Center>
