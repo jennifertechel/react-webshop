97a57117ea2b205064c31476ec90f22d6b6cb30c
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-13 10:39:13 +0100

    Spacing, margins and background color of productCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,37 +21,43 @@ function ProductCard() {
         overflow="hidden"
         boxShadow="none"
         borderRadius="0"
+        margin="30px"
+        bg="transparent"
       >
-        <Image
-          objectFit="cover"
-          maxW={{ base: "100%", sm: "800px" }}
-          src={product.image}
-          alt={product.title}
-        />
+        <Flex flex={1}>
+          <Image
+            objectFit="cover"
+            width="100%"
+            maxW={{ base: "100%", sm: "100%" }}
+            src={product.image}
+            alt={product.title}
+          />
+        </Flex>
+        <Flex flex={1}>
+          <Stack>
+            <CardBody>
+              <Heading size="md">{product.title}</Heading>
+              <Text>Höjd:{product.height}</Text>
 
-        <Stack>
-          <CardBody>
-            <Heading size="md">{product.title}</Heading>
-            <Text>Höjd:{product.height}</Text>
+              <Text py="2">{product.description}</Text>
 
-            <Text py="2">{product.description}</Text>
+              <Text> {product.price} SEK</Text>
+            </CardBody>
 
-            <Text> {product.price}</Text>
-          </CardBody>
-
-          <CardFooter>
-            <Button
-              variant="outline"
-              colorScheme="yellow"
-              borderColor="yellow.400"
-              color="black"
-              borderRadius="none"
-              borderWidth="2px"
-            >
-              Lägg till i varukorgen
-            </Button>
-          </CardFooter>
-        </Stack>
+            <CardFooter>
+              <Button
+                variant="outline"
+                colorScheme="yellow"
+                borderColor="yellow.400"
+                color="black"
+                borderRadius="none"
+                borderWidth="2px"
+              >
+                Lägg till i varukorgen
+              </Button>
+            </CardFooter>
+          </Stack>
+        </Flex>
       </Card>
     </Flex>
   );
