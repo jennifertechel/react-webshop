fa52e48e6b324eed4f36e74df0819259b17c0a54
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-13 10:05:55 +0100

    Created productcard component

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,12 @@
+import ProductCard from "../components/ProductCard";
+
 function Product() {
-  return <h1>This is a detailed product page</h1>;
+  return (
+    <div>
+      <h1>This is a detailed product page</h1>
+      <ProductCard />
+    </div>
+  );
 }
 
 export default Product;
