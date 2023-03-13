29eb59c11905771e670cd3d0588b624caa001a8f
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-13 11:22:13 +0100

    Added buttons

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
@@ -16,7 +17,7 @@ function ProductCard() {
   return (
     <Flex justifyContent="center" alignItems="center">
       <Card
-        maxW="700px"
+        maxW="800px"
         direction={{ base: "column", sm: "row" }}
         overflow="hidden"
         boxShadow="none"
@@ -34,14 +35,20 @@ function ProductCard() {
           />
         </Flex>
         <Flex flex={1}>
-          <Stack>
+          <Stack maxW="450px" spacing="10px">
             <CardBody>
-              <Heading size="md">{product.title}</Heading>
-              <Text>Höjd:{product.height}</Text>
+              <Heading margin="3px 10px 10px 10px" fontSize="xl">
+                {product.title}{" "}
+              </Heading>
+              <Text margin="3px 10px 10px 10px">Höjd:{product.height}</Text>
 
-              <Text py="2">{product.description}</Text>
+              <Text margin="3px 10px 10px 10px" py="2">
+                {product.description}
+              </Text>
 
-              <Text> {product.price} SEK</Text>
+              <Text fontSize="xl" margin="3px 10px 10px 10px">
+                {product.price} SEK
+              </Text>
             </CardBody>
 
             <CardFooter>
@@ -52,9 +59,42 @@ function ProductCard() {
                 color="black"
                 borderRadius="none"
                 borderWidth="2px"
+                mr="2"
+                height="50px"
+                _hover={{ bg: "yellow.400" }}
               >
                 Lägg till i varukorgen
               </Button>
+              <Box
+                borderWidth="2px"
+                borderColor="yellow.400"
+                borderRadius="none"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                width="140px"
+              >
+                <Button
+                  bg="transparent"
+                  colorScheme="yellow"
+                  borderColor="transparent"
+                  borderWidth="2px"
+                  borderRadius="none"
+                  _hover={{ bg: "yellow.400" }}
+                >
+                  -
+                </Button>
+                <Text mx="2">0</Text>
+                <Button
+                  borderColor="transparent"
+                  bg="transparent"
+                  borderWidth="2px"
+                  borderRadius="none"
+                  _hover={{ bg: "yellow.400" }}
+                >
+                  +
+                </Button>
+              </Box>
             </CardFooter>
           </Stack>
         </Flex>
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -3,7 +3,6 @@ import ProductCard from "../components/ProductCard";
 function Product() {
   return (
     <div>
-      <h1>This is a detailed product page</h1>
       <ProductCard />
     </div>
   );
