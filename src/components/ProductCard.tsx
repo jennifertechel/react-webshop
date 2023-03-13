f346bd517931279fdbdcee185125c9866f9519d9
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-13 10:23:07 +0100

    Added image from data and some styling

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,43 +3,57 @@ import {
   Card,
   CardBody,
   CardFooter,
+  Flex,
   Heading,
   Image,
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { products } from "../../data";
 
 function ProductCard() {
+  const product = products[0];
   return (
-    <Card
-      direction={{ base: "column", sm: "row" }}
-      overflow="hidden"
-      variant="outline"
-    >
-      <Image
-        objectFit="cover"
-        maxW={{ base: "100%", sm: "200px" }}
-        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-        alt="Caffe Latte"
-      />
+    <Flex justifyContent="center" alignItems="center">
+      <Card
+        maxW="700px"
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        boxShadow="none"
+        borderRadius="0"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src={product.image}
+          alt="Caffe Latte"
+        />
 
-      <Stack>
-        <CardBody>
-          <Heading size="md">The perfect latte</Heading>
+        <Stack>
+          <CardBody>
+            <Heading size="md">The perfect latte</Heading>
 
-          <Text py="2">
-            Caffè latte is a coffee beverage of Italian origin made with
-            espresso and steamed milk.
-          </Text>
-        </CardBody>
+            <Text py="2">
+              Caffè latte is a coffee beverage of Italian origin made with
+              espresso and steamed milk.
+            </Text>
+          </CardBody>
 
-        <CardFooter>
-          <Button variant="solid" colorScheme="blue">
-            Buy Latte
-          </Button>
-        </CardFooter>
-      </Stack>
-    </Card>
+          <CardFooter>
+            <Button
+              variant="outline"
+              colorScheme="yellow"
+              borderColor="yellow.400"
+              color="black"
+              borderRadius="none"
+              borderWidth="2px"
+            >
+              Buy Latte
+            </Button>
+          </CardFooter>
+        </Stack>
+      </Card>
+    </Flex>
   );
 }
 
