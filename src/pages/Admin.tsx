e3a3b3a1c0fb997e7b0ada482bcf313cd41a3f01
ye-mou <git.ymo22@gmail.com>
2023-03-15 19:30:56 +0100

    Added buttons and layout for the admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardBody,
@@ -8,53 +9,91 @@ import {
   Image,
   Stack,
   StackDivider,
+  Text,
 } from "@chakra-ui/react";
 import { products } from "../../data";
 import { Currency } from "../components/Currency";
 
 function Admin() {
-  const product = products[2];
   return (
-    <Stack direction={{ base: "column", md: "row" }} spacing="4">
-      <Card
-        direction={{ base: "column", sm: "row" }}
-        overflow="hidden"
-        variant="outline"
+    <Box>
+      <Box>Admin</Box>
+
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        h="200px"
+        color="black"
+        fontSize="2xl"
+        px="4"
+        position="fixed"
+        top="0"
+        bg="#fff"
+        w="100%"
+        zIndex="1"
       >
-        <Image
-          objectFit="cover"
-          maxW={{ base: "100%", sm: "200px" }}
-          src={product.image}
-          alt={product.title}
-        />
+        <Button
+          border="1px"
+          padding="8px"
+          color="#c2a77b"
+          variant="link"
+          ml="auto"
+          mt="50px"
+        >
+          Lägg till ny produkt
+        </Button>
+      </Flex>
 
-        <Stack spacing="4">
-          <CardBody>
-            <Stack divider={<StackDivider />}>
-              <Heading size="md">{product.title}</Heading>
-            </Stack>
-          </CardBody>
+      <Stack direction="column" spacing="4">
+        {products.map((product) => (
+          <Card key={product.id} variant="outline" overflow="hidden">
+            <Image
+              objectFit="cover"
+              maxH="300px"
+              maxW="300px"
+              src={product.image}
+              alt={product.title}
+              style={{ margin: "auto" }}
+            />
 
-          <CardFooter>
-            <Flex justifyContent="flex-end">
-              <Stack direction="column" spacing="2">
-                <p>{product.description}</p>
-                <Button bg="#e1dacf" color="black" variant="solid">
-                  Edit
-                </Button>
-                <Button bg="#e1dacf" color="black" variant="outline">
-                  Ta bort
-                </Button>
+            <CardBody>
+              <Stack divider={<StackDivider />}>
+                <Heading size="md">{product.title}</Heading>
+                <Text>{product.description}</Text>
               </Stack>
-            </Flex>
-          </CardFooter>
+              <Text fontWeight="bold" fontSize="sm">
+                {Currency(product.price)}
+              </Text>
+            </CardBody>
 
-          <CardFooter mt="4rem">
-            <p>{Currency(product.price)}</p>
-          </CardFooter>
-        </Stack>
-      </Card>
-    </Stack>
+            <CardFooter>
+              <Flex justifyContent="space-between" alignItems="center">
+                <Text fontWeight="bold" fontSize="lg"></Text>
+
+                <Stack direction="row" spacing="5" ml="60">
+                  <Button
+                    border="1px"
+                    padding="8px"
+                    color="#c2a77b"
+                    variant="link"
+                  >
+                    Redigera
+                  </Button>
+                  <Button
+                    border="1px"
+                    padding="8px"
+                    color="#c2a77b"
+                    variant="link"
+                  >
+                    Ta bort
+                  </Button>
+                </Stack>
+              </Flex>
+            </CardFooter>
+          </Card>
+        ))}
+      </Stack>
+    </Box>
   );
 }
 
