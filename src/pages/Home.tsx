e10bf8162c17afa7871e57b79ed3cf99586dfdb1
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-15 13:51:04 +0100

    added fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,26 @@
-import { Box, Center, Grid, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { products } from "../../data";
 import CardHome from "../components/Card";
 
 function Home() {
   return (
     <div>
-      <Box pos="relative">
-        <Image src="/public/hero-md.svg" />
+      <Box pos='relative'>
+        <Image src='/public/hero-md.svg' />
         <Box
-          position="absolute"
-          top="50%"
-          left="25%"
-          transform="translate(-50%, -25%)"
+          position='absolute'
+          top='50%'
+          left='25%'
+          transform='translate(-50%, -25%)'
         >
           <Text>Handmade ceramic vases</Text>
           <Heading>Made with love and stuff</Heading>
@@ -22,11 +31,15 @@ function Home() {
           <Heading>Alla Produkter</Heading>
         </Center>
       </Box>
-      <Grid templateColumns="repeat(3, 1fr)" gap={6} py={8}>
-        {products.map((product) => (
-          <CardHome key={product.id} product={product} />
-        ))}
-      </Grid>
+      <Center>
+        <Grid templateColumns='repeat(3, 1fr)' columnGap={24} rowGap={8} py={8}>
+          {products.map((product) => (
+            <GridItem>
+              <CardHome key={product.id} product={product} />
+            </GridItem>
+          ))}
+        </Grid>
+      </Center>
     </div>
   );
 }
