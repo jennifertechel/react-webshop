d502de3173a2d0af5eb1e09cd65e86d4591d2f69
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-21 14:25:50 +0100

    add  to cart from home page button

--- src/components/OverviewCard.tsx
+++ src/components/OverviewCard.tsx
@@ -7,52 +7,70 @@ import {
   Icon,
   Image,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { Product } from "../../data";
+import { useCart } from "../context/cartContext";
 
 function OverviewCard({ product }: { product: Product }) {
+  const { addToCart } = useCart();
+  const toast = useToast();
+
+  const handleAddToCart = (item: Product) => {
+    addToCart(item);
+    toast({
+      // Call the useToast hook to show a toast notification
+      title: "Product added to cart",
+      status: "success",
+      duration: 1000,
+      isClosable: false,
+    });
+  };
+
   return (
-    <Link to={"/product/" + product.id} key={product.id}>
-      <Card
-        data-cy="product"
-        variant="unstyled"
-        maxW="xs"
-        align="center"
-        bg="brand.100"
-        p={4}
-        _hover={{
-          boxShadow: "2xl",
-        }}
-      >
-        <CardBody>
+    <Card
+      data-cy="product"
+      variant="unstyled"
+      maxW="xs"
+      align="center"
+      bg="brand.100"
+      p={4}
+      _hover={{
+        boxShadow: "2xl",
+      }}
+    >
+      <CardBody>
+        <Link to={"/product/" + product.id} key={product.id}>
           <Image src={product.image} alt={product.title} />
-          <Flex justifyContent="space-between" pt={3}>
-            <Box>
-              <Text data-cy="product-title">{product.title}</Text>
-              <Text data-cy="product-price">
-                {product.price}
-                <Text as="span" fontSize="xs">
-                  &nbsp;SEK
-                </Text>
+        </Link>
+        <Flex justifyContent="space-between" pt={3}>
+          <Box>
+            <Text data-cy="product-title">{product.title}</Text>
+            <Text data-cy="product-price">
+              {product.price}
+              <Text as="span" fontSize="xs">
+                &nbsp;SEK
               </Text>
-            </Box>
-            <Button
-              data-cy="product-buy-button"
-              variant="outline"
-              colorScheme="orange"
-              border="none"
-              color="black"
-              borderRadius="none"
-              _hover={{ bg: "orange.100" }}
-            >
-              <Icon boxSize={6} as={IoMdAdd} />
-            </Button>
-          </Flex>
-        </CardBody>
-      </Card>
-    </Link>
+            </Text>
+          </Box>
+
+          <Button
+            data-cy="product-buy-button"
+            variant="outline"
+            colorScheme="orange"
+            border="none"
+            color="black"
+            borderRadius="none"
+            onClick={() => handleAddToCart(product)}
+            _hover={{ bg: "orange.100" }}
+          >
+            <Icon boxSize={6} as={IoMdAdd} />
+          </Button>
+        </Flex>
+      </CardBody>
+    </Card>
   );
 }
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,8 +11,8 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { Product } from "../../data";
-import { Currency } from "./Currency";
 import { useCart } from "../context/cartContext";
+import { Currency } from "./Currency";
 
 function ProductCard({ product }: { product: Product }) {
   const { addToCart } = useCart();
@@ -29,50 +29,50 @@ function ProductCard({ product }: { product: Product }) {
     });
   };
   return (
-    <Flex justifyContent='center' alignItems='center'>
+    <Flex justifyContent="center" alignItems="center">
       <Card
-        maxW='800px'
+        maxW="800px"
         direction={{ base: "column", sm: "row" }}
-        overflow='hidden'
-        boxShadow='none'
-        borderRadius='0'
-        margin='30px'
-        bg='transparent'
+        overflow="hidden"
+        boxShadow="none"
+        borderRadius="0"
+        margin="30px"
+        bg="transparent"
       >
         <Flex flex={1}>
           <Image
-            objectFit='cover'
-            width='100%'
+            objectFit="cover"
+            width="100%"
             maxW={{ base: "100%", sm: "100%" }}
-            minW='200px'
+            minW="200px"
             src={product.image}
             alt={product.title}
           />
         </Flex>
-        <Flex flex={1} alignItems='center' justifyContent='space-between'>
-          <Stack maxW='450px' spacing='5px'>
+        <Flex flex={1} alignItems="center" justifyContent="space-between">
+          <Stack maxW="450px" spacing="5px">
             <CardBody>
               <Heading
-                data-cy='product-title'
-                margin='3px 10px 10px 10px'
-                fontSize='xl'
+                data-cy="product-title"
+                margin="3px 10px 10px 10px"
+                fontSize="xl"
               >
                 {product.title}
               </Heading>
-              <Text margin='3px 10px 10px 10px'>Höjd:{product.height}</Text>
+              <Text margin="3px 10px 10px 10px">Höjd:{product.height}</Text>
 
               <Text
-                data-cy='product-description'
-                margin='3px 10px 10px 10px'
-                py='2'
+                data-cy="product-description"
+                margin="3px 10px 10px 10px"
+                py="2"
               >
                 {product.description}
               </Text>
 
               <Text
-                data-cy='product-price'
-                fontSize='xl'
-                margin='3px 10px 10px 10px'
+                data-cy="product-price"
+                fontSize="xl"
+                margin="3px 10px 10px 10px"
               >
                 {Currency(product.price)}
               </Text>
@@ -81,54 +81,20 @@ function ProductCard({ product }: { product: Product }) {
             <CardFooter flexDirection={{ base: "column", md: "row" }}>
               <Button
                 onClick={() => handleAddToCart(product)}
-                data-cy='product-buy-button'
-                variant='outline'
-                colorScheme='yellow'
-                borderColor='yellow.400'
-                color='black'
-                borderRadius='none'
-                borderWidth='2px'
-                mr='2'
-                height='45px'
+                data-cy="product-buy-button"
+                variant="outline"
+                colorScheme="yellow"
+                borderColor="yellow.400"
+                color="black"
+                borderRadius="none"
+                borderWidth="2px"
+                mr="2"
+                height="45px"
                 _hover={{ bg: "yellow.400" }}
                 m={{ base: "10px", sm: "2px" }}
               >
                 Lägg till i varukorgen
               </Button>
-              {/* <Box
-                borderWidth="2px"
-                borderColor="yellow.400"
-                borderRadius="none"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                width="140px"
-                height="45px"
-                m={{ base: "10px", sm: "2px" }}
-              >
-                <Button
-                  bg="transparent"
-                  colorScheme="yellow"
-                  borderColor="transparent"
-                  borderWidth="2px"
-                  borderRadius="none"
-                  _hover={{ bg: "yellow.400" }}
-                  height="45px"
-                >
-                  -
-                </Button>
-                <Text mx="2">0</Text>
-                <Button
-                  borderColor="transparent"
-                  bg="transparent"
-                  borderWidth="2px"
-                  borderRadius="none"
-                  _hover={{ bg: "yellow.400" }}
-                  height="45px"
-                >
-                  +
-                </Button>
-              </Box> */}
             </CardFooter>
           </Stack>
         </Flex>
