0978732e6ba8f33933e36b60a8c16d95900878eb
MadeleineGustafson <madeleinegu@gmail.com>
2023-03-21 11:11:54 +0100

    fixed deleted quantity in cart and added data-cy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Image, Icon, Badge } from "@chakra-ui/react";
-import { BsPerson, BsBag } from "react-icons/bs";
+import { Badge, Box, Flex, Icon, Image } from "@chakra-ui/react";
+import { BsBag, BsPerson } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/cartContext";
 
@@ -11,37 +11,38 @@ function Header() {
 
   return (
     <Flex
-      as='header'
+      as="header"
       p={4}
-      borderBottom='1px'
-      borderColor='blackAlpha.200'
-      bg='brand.100'
-      position='fixed'
-      zIndex='sticky'
-      width='100%'
-      justifyContent='space-between'
+      borderBottom="1px"
+      borderColor="blackAlpha.200"
+      bg="brand.100"
+      position="fixed"
+      zIndex="sticky"
+      width="100%"
+      justifyContent="space-between"
     >
-      <Box mx='auto'>
-        <Link to='/'>
-          <Image src='logo.svg' maxWidth='150px' maxHeight='100%' />
+      <Box mx="auto">
+        <Link to="/">
+          <Image src="logo.svg" maxWidth="150px" maxHeight="100%" />
         </Link>
       </Box>
-      <Flex alignItems='center'>
-        <Link to='admin'>
+      <Flex alignItems="center">
+        <Link to="admin">
           <Icon boxSize={8} as={BsPerson} />
         </Link>
 
-        <Link to='checkout'>
-          <Box pos='relative'>
+        <Link to="checkout" data-cy="cart-link">
+          <Box pos="relative">
             <Icon boxSize={8} as={BsBag} />
             {totalQuantity > 0 && (
               <Badge
-                position='absolute'
-                top='-10px'
-                right='-6px'
-                colorScheme='yellow'
-                fontSize='0.85rem'
-                borderRadius='10px'
+                position="absolute"
+                top="-10px"
+                right="-6px"
+                colorScheme="yellow"
+                fontSize="0.85rem"
+                borderRadius="10px"
+                data-cy="cart-items-count-badge"
               >
                 {totalQuantity}
               </Badge>
