e9176c5568ed9e2484e4a31e7a22b42dacfdfbca
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-28 17:39:20 +0200

    cleanup

--- src/components/AdminCard.tsx
+++ src/components/AdminCard.tsx
@@ -15,21 +15,21 @@ import AlertDialogDelete from "../components/AlertDialog";
 import { Currency } from "../components/Currency";
 import { useProducts } from "../context/productContext";
 function AdminCard() {
-  const { products, deleteProduct } = useProducts();
+  const { products } = useProducts();
   return (
-    <Box bg="brand.900" py={2}>
-      <Box maxW="1200px" mx="auto">
-        <Flex justifyContent="space-between" alignItems="center">
-          <Heading color="Black" fontSize="xl">
+    <Box bg='brand.900' py={2}>
+      <Box maxW='1200px' mx='auto'>
+        <Flex justifyContent='space-between' alignItems='center'>
+          <Heading color='Black' fontSize='xl'>
             Admin
           </Heading>
-          <Link to="/add-product">
+          <Link to='product/new'>
             <Button
-              data-cy="admin-add-product"
-              border="1px"
-              padding="4px"
-              color="#c2a77b"
-              variant="link"
+              data-cy='admin-add-product'
+              border='1px'
+              padding='4px'
+              color='#c2a77b'
+              variant='link'
             >
               Lägg till ny produkt
             </Button>
@@ -37,44 +37,44 @@ function AdminCard() {
         </Flex>
       </Box>
 
-      <Box maxW="1200px" mx="auto" mt={4}>
+      <Box maxW='1200px' mx='auto' mt={4}>
         {products.map((product) => (
           <Card
-            data-cy="product"
+            data-cy='product'
             key={product.id}
-            variant="outline"
-            overflow="hidden"
-            bg="brand.100"
+            variant='outline'
+            overflow='hidden'
+            bg='brand.100'
             mt={2}
           >
             <Flex>
               <Image
-                objectFit="cover"
-                maxH="300px"
-                maxW="300px"
+                objectFit='cover'
+                maxH='300px'
+                maxW='300px'
                 src={product.image}
                 alt={product.title}
                 style={{ margin: "auto" }}
               />
 
-              <Flex direction="column" justify="center" ml="4">
-                <Heading data-cy="product-title" size="md">
+              <Flex direction='column' justify='center' ml='4'>
+                <Heading data-cy='product-title' size='md'>
                   {product.title}
                 </Heading>
                 <Text>{product.description}</Text>
-                <Text data-cy="product-id">{product.id}</Text>
+                <Text data-cy='product-id'>{product.id}</Text>
 
-                <Text data-cy="product-price" fontWeight="bold" fontSize="sm">
+                <Text data-cy='product-price' fontWeight='bold' fontSize='sm'>
                   {Currency(product.price)}
                 </Text>
-                <Flex justifyContent="space-between" alignItems="center">
-                  <Text fontWeight="bold" fontSize="lg"></Text>
+                <Flex justifyContent='space-between' alignItems='center'>
+                  <Text fontWeight='bold' fontSize='lg'></Text>
                   <AlertDialogDelete productId={product.id} />
 
-                  <Stack direction="row" spacing="5">
-                    <Link to="/edit">
+                  <Stack direction='row' spacing='5'>
+                    <Link to={"product/" + product.id}>
                       <Icon
-                        data-cy="admin-edit-product"
+                        data-cy='admin-edit-product'
                         boxSize={7}
                         as={AiOutlineEdit}
                       />
