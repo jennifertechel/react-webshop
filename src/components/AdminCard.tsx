85a9adce6576fd4a42bd65b1dd8fff39a7831c23
Jennifer Techel <jennifertechel@hotmail.com>
2023-03-29 09:54:57 +0200

    styling changed after merge

--- src/components/AdminCard.tsx
+++ src/components/AdminCard.tsx
@@ -1,6 +1,6 @@
 import {
   Card,
-  Center,
+  Box,
   Flex,
   Heading,
   Icon,
@@ -12,127 +12,88 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { Product } from "../../data";
 import AlertDialogDelete from "../components/AlertDialog";
-import { Currency } from "../components/Currency";
 
 function AdminCard({ product }: { product: Product }) {
   return (
-    <Center>
-      <Box w='50%'>
-        <Flex
-          justifyContent='space-between'
-          alignItems='center'
-          mb={4}
-          borderBottom='1px solid black'
-          direction={{ base: "column", md: "row" }}
-        >
-          <Heading as='h5' size='sm' mb={{ base: 5 }}>
-            Alla produkter
-          </Heading>
-          <Link to='/add-product'>
-            <Button
-              data-cy='admin-add-product'
-              colorScheme='yellow'
-              bg='base.100'
-              borderColor='yellow.400'
-              color='black'
-              borderRadius='none'
-              borderWidth='1.5px'
-              variant='solid'
-              size='sm'
-              w='13rem'
-              mb={5}
-              h='3rem'
-              _hover={{ bg: "orange.100" }}
-            >
-              Lägg till ny produkt
-            </Button>
-          </Link>
-        </Flex>
-        <Stack spacing={6} w='100%'>
-          {products.map((product) => (
-            <Card
-              data-cy='product'
-              key={product.id}
-              direction={{ base: "column", sm: "row" }}
-              overflow='hidden'
-              bg='brand.100'
-              variant='unstyled'
-              my={2}
-              borderBottom='1px'
-              borderColor='blackAlpha.200'
-              pb={4}
-              borderRadius='0'
-            >
-              <Flex direction={{ base: "column", md: "row" }} flex='1'>
-                <Image
-                  objectFit='cover'
-                  maxW={{ base: "100%", md: "200px" }}
-                  w='auto'
-                  src={product.image}
-                  alt={product.title}
-                />
+    <Card
+      data-cy='product'
+      key={product.id}
+      direction={{ base: "column", sm: "row" }}
+      overflow='hidden'
+      bg='brand.100'
+      variant='unstyled'
+      my={2}
+      borderBottom='1px'
+      borderColor='blackAlpha.200'
+      pb={4}
+      borderRadius='0'
+    >
+      <Flex direction={{ base: "column", md: "row" }} flex='1'>
+        <Image
+          objectFit='cover'
+          maxW={{ base: "100%", md: "200px" }}
+          w='auto'
+          src={product.image}
+          alt={product.title}
+        />
 
-                <Stack
-                  p={4}
-                  justifyContent='space-between'
-                  alignItems='stretch'
-                  flex='1'
-                >
-                  <Flex
-                    direction='row'
-                    justifyContent={{ base: "center", md: "space-between" }}
-                  >
-                    <Box>
-                      <Heading data-cy='product-title' as='h3' size='md' mb={2}>
-                        {product.title}
-                      </Heading>
-                      <Flex>
-                        <Text>Id: </Text>
-                        <Text data-cy='product-id' mb={4}>
-                          {product.id}
-                        </Text>
-                      </Flex>
-                      <Flex>
-                        <Text>Höjd: </Text>
-                        <Text mb={4}>{product.height}</Text>
-                      </Flex>
-                      <Flex>
-                        <Text>Pris: </Text>
-                        <Text data-cy='product-price' mb={4}>
-                          {product.price}
-                        </Text>
-                      </Flex>
-                    </Box>
-                  </Flex>
-                </Stack>
-                <Box mt='5'>
-                  <Flex
-                    justifyContent={{ base: "center", md: "space-between" }}
-                    alignItems='flex-end'
-                    direction={{ base: "row", md: "column" }}
-                  >
-                    <Link to='/edit'>
-                      <Icon
-                        bg='base.100'
-                        color='black'
-                        borderRadius='none'
-                        data-cy='admin-edit-product'
-                        boxSize={9}
-                        as={AiOutlineEdit}
-                        mb={{ base: -1, md: 7 }}
-                        mr={{ base: 10, md: 0 }}
-                        _hover={{ bg: "orange.100" }}
-                      />
-                    </Link>
-                    <AlertDialogDelete productId={product.id} />
-                  </Flex>
-                </Box>
+        <Stack
+          p={4}
+          justifyContent='space-between'
+          alignItems='stretch'
+          flex='1'
+        >
+          <Flex
+            direction='row'
+            justifyContent={{ base: "center", md: "space-between" }}
+          >
+            <Box>
+              <Heading data-cy='product-title' as='h3' size='md' mb={2}>
+                {product.title}
+              </Heading>
+              <Flex>
+                <Text>Id:&nbsp; </Text>
+                <Text data-cy='product-id' mb={4}>
+                  {product.id}
+                </Text>
               </Flex>
-            </Card>
-          ))}
+              <Flex>
+                <Text>Höjd:&nbsp;</Text>
+                <Text mb={4}>{product.height}</Text>
+              </Flex>
+              <Flex>
+                <Text>Pris:&nbsp;</Text>
+                <Text data-cy='product-price' mb={4}>
+                  {product.price}
+                </Text>
+              </Flex>
+            </Box>
+          </Flex>
         </Stack>
-      </Box>
-    </Center>
+        <Box mt='5'>
+          <Flex
+            justifyContent={{ base: "center", md: "space-between" }}
+            alignItems='flex-end'
+            direction={{ base: "row", md: "column" }}
+          >
+            <Link to='/edit'>
+              <Icon
+                bg='base.100'
+                color='black'
+                borderRadius='none'
+                data-cy='admin-edit-product'
+                boxSize={9}
+                as={AiOutlineEdit}
+                mb={{ base: -1, md: 7 }}
+                mr={{ base: 10, md: 0 }}
+                _hover={{ bg: "orange.100" }}
+              />
+            </Link>
+            <AlertDialogDelete productId={product.id} />
+          </Flex>
+        </Box>
+      </Flex>
+    </Card>
   );
 }
 
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -2,7 +2,6 @@ import { Box, Center, Heading } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ProductForm from "../components/ProductForm";
 import { useProducts } from "../context/productContext";
-import { useProducts } from "../context/productContext";
 
 function Edit() {
   const { products } = useProducts();
